0823238d15776f3e71d931b32391a7f9f8a53153
Andrii Maksymov <maximov.echo@gmail.com>
2020-05-23 00:28:05 +0300

    Add empty test

--- tests/test_wags.py
+++ tests/test_wags.py
@@ -1,5 +1,12 @@
 from wags import __version__
+from wags.applications import Wags 
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_applications():
+    app = Wags()
+    assert True
+
